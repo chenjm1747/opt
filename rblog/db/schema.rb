97fb432d2afd649711a260dd61fde8913b7993aa
ajim <2100803@qq.com>
2015-05-19 15:27:13 +0800

    autocommit

--- rblog/db/schema.rb
+++ rblog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518230401) do
+ActiveRecord::Schema.define(version: 20150519062023) do
 
   create_table "azufangs", force: :cascade do |t|
     t.string   "username"
@@ -140,6 +140,7 @@ ActiveRecord::Schema.define(version: 20150518230401) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
